785ff519852fb77f042eb07c7a056e7050b21ce2
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2016-02-27 10:56:38 +0100

    None

---
+++ src/osvPandas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import networkx as nx
+import re
+
+tabby=pd.read_csv('../stp/Tabby2.csv',sep=';')
+points = tabby[tabby['txt'].str.contains("CARTESIAN_POINT")==True]
+points['txt'] = points['txt'].str.replace("CARTESIAN_POINT\('',",'')
+points['txt'] = points['txt'].str.replace("\)\)",')')
+pattern = '#[0-9]*'
+tabby['nodes']=map(lambda x : re.findall(pattern,x),tabby['txt'].values)
+pt = map(lambda x : eval(x),points['txt'].values)
+G = nx.Graph()
+G.add_nodes_from(tabby['#ID'].values)
